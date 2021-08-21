d5712431e4d1984fc6b6b4886874ebcad941dff4
blacktop12 <57530914+blacktop12@users.noreply.github.com>
2021-08-21 12:12:15 -0500

    Get and Post v2

--- src/main/java/com/demo/demoapi/config/SwaggerConfig.java
+++ src/main/java/com/demo/demoapi/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
