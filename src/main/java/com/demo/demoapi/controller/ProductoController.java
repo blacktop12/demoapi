6d2d46e83e20286cf074dc0acd20a29f22d06cd6
blacktop12 <57530914+blacktop12@users.noreply.github.com>
2021-08-21 12:00:56 -0500

    Get and Post

--- src/main/java/com/demo/demoapi/controller/ProductoController.java
+++ src/main/java/com/demo/demoapi/controller/ProductoController.java
@@ -1,9 +1,13 @@
 package com.demo.demoapi.controller;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import com.demo.demoapi.model.Producto;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
@@ -12,34 +16,33 @@ import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping(value = "demoapi/product", produces = "application/json")
-
 public class ProductoController {
     private Map<String, Producto> products;
 
     public ProductoController() {
-        /*
-        private String id;
-        private String pname;
-        private float price;
-        private int stock;
-        private Date createdat;
-        */
 
         products = new HashMap<String, Producto>();
         Producto p = new Producto();
         
-        p.setEmail("email@email.com");
-        p.setName("name");
-        p.setPhoneNumber("999999999");
-        p.setBirthDate(new Date());
-        String id = UUID.randomUUID().toString();
+        p.setPname("Producto 1");
+        p.setPrice(10);
+        p.setStock(100);
+        p.setCreatedat(new Date());
+
+        String id=UUID.randomUUID().toString();
         p.setId(id);
-        participantes.put(id, p);
-
+        products.put(id, p);
+                
     }
-
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Producto>> list() {
         return new ResponseEntity<Map<String, Producto>>(products, HttpStatus.OK);
+    }
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<String> add(@RequestBody Producto p) {
+        String id = UUID.randomUUID().toString();
+        p.setId(id);
+        products.put(id, p);
+        return new ResponseEntity<String>(id, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/demo/demoapi/model/Producto.java
+++ src/main/java/com/demo/demoapi/model/Producto.java
@@ -14,5 +14,45 @@ public class Producto
     private float price;
     private int stock;
     private Date createdat;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPname() {
+        return this.pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Date getCreatedat() {
+        return this.createdat;
+    }
+
+    public void setCreatedat(Date createdat) {
+        this.createdat = createdat;
+    }
 }
 
